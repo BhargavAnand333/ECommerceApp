f1aabe06c97819b9093c7182a453c057113bed81
BhargavAnand333 <bhargav2003anand@gmail.com>
2025-01-21 20:50:34 +0530

    Updated logos

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,16 +1,17 @@
-import logo from './logo.png'
-import add_icon from './add_icon.png'
-import order_icon from './order_icon.png'
-import upload_area from './upload_area.png'
-import parcel_icon from './parcel_icon.svg'
-import p_img2_1 from './p_img2_1.png'
+import logo from "./logo.png";
+import add_icon from "./add_icon.png";
+import order_icon from "./order_icon.png";
+import upload_area from "./upload_area.png";
+import parcel_icon from "./parcel_icon.svg";
+import p_img2_1 from "./p_img2_1.png";
+import new_logo_3 from "./new_logo_3.png";
 
 export const assets = {
-    logo,
-    add_icon,
-    order_icon,
-    upload_area,
-    parcel_icon,
-    p_img2_1
-}
-
+  new_logo_3,
+  logo,
+  add_icon,
+  order_icon,
+  upload_area,
+  parcel_icon,
+  p_img2_1,
+};
